cc4457ca6e9e3ae49ef30c05f82a694683117e3d
jonathan-gartland <jonathan-gartland@gmail.com>
2024-08-09 18:54:20 -0400

    changes - fix typo

--- src/app/ui/sidenav.tsx
+++ src/app/ui/sidenav.tsx
@@ -2,7 +2,7 @@ import NavLinks from "@/app/ui/nav-links";
 
 export default function SideNav() {
   return (
-    <div className="w-full flex-start md:w-64">ga
+    <div className="w-full flex-start md:w-64">
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
       </div>
